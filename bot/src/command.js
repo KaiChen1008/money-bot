59113c8a2bbe17c1883c40c587325d0b8cb94181
KaiChen1008 <1038790@gmail.com>
2021-09-21 00:55:03 +0800

    fix indentation to tab

--- bot/src/command.js
+++ bot/src/command.js
@@ -13,31 +13,31 @@ let liner;
 * Calculate monthly expense
 */
 function sumMonthlyExpense() {
-		liner = new lineByLine(RECORD_FILE);
-		total = 0;
-		var c = DateTime.now().setZone('Asia/Taipei').c
-		currMonth = c.month;
-		currYear = c.year;
-		var y,m,d,h,min,item,price;
-		var _d = null;
-		var countD = 0;
-	
-		// get all record
-		while (line = liner.next()) {
-			line = line.toString();
-			[y,m,d,h,min,item,price] = line.split(",");
-			if (m == currMonth) {
-				total += parseInt(price);
-			}
-	
-			if (_d == null || d != _d) {
-				countD ++;
-				_d = d;
-			}
+	liner = new lineByLine(RECORD_FILE);
+	total = 0;
+	var c = DateTime.now().setZone('Asia/Taipei').c
+	currMonth = c.month;
+	currYear = c.year;
+	var y,m,d,h,min,item,price;
+	var _d = null;
+	var countD = 0;
+
+	// get all record
+	while (line = liner.next()) {
+		line = line.toString();
+		[y,m,d,h,min,item,price] = line.split(",");
+		if (m == currMonth) {
+			total += parseInt(price);
 		}
-	
-		console.info(`[INFO] Total ${total} in ${MONTHS[parseInt(m)]}`);
-		return `You have spent ${total} in ${MONTHS[parseInt(m)]}, ${(total/countD).toFixed(2)}/day`;
+
+		if (_d == null || d != _d) {
+			countD ++;
+			_d = d;
+		}
+	}
+
+	console.info(`[INFO] Total ${total} in ${MONTHS[parseInt(m)]}`);
+	return `You have spent ${total} in ${MONTHS[parseInt(m)]}, ${(total/countD).toFixed(2)}/day`;
 }
 
 /**
